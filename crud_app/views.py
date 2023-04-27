5671f7c95f21f751d1a35d7eb9ffd034fd1019d6
NisioH <nisiohelandeoliveira@gmail.com>
2023-04-27 10:33:20 -0400

    adição de templates

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from crud_app.models import Posts
 
 # Create your views here.
+def post_list(request):
+    tamplate_name = 'post_list.html'
+    posts = Posts.objects.all() # query para todas as postagens
+    context = {
+        'posts': posts
+    }
+    return render(request, tamplate_name, context)
